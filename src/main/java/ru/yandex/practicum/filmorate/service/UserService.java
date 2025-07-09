8dc6cfeb6ce593053ee71576ed192796f3e2969d
Vitaly-Trepalin <vitalietrepalin@yandex.ru>
2025-07-09 21:07:13 +0300

    Добавить рекомендации по фильмам для просмотра

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -7,9 +7,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.friendship.FriendshipStorage;
 import ru.yandex.practicum.filmorate.storage.users.UserStorage;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 @Service
