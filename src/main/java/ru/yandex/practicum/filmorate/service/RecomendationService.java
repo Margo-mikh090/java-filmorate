55e63a66896ff149d8eb844bfb2d769a5d345ef6
Vitaly-Trepalin <vitalietrepalin@yandex.ru>
2025-07-09 21:07:13 +0300

    Добавить рекомендации по фильмам для просмотра

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/RecomendationService.java
@@ -0,0 +1,65 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.films.FilmDbStorage;
+import ru.yandex.practicum.filmorate.storage.likes.LikeDbStorage;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RecomendationService {
+    private final LikeDbStorage likeDbStorage;
+    private final FilmDbStorage filmDbStorage;
+
+    public List<Film> getRecommendations(long userId) {
+        log.info("Запрос на получение списка рекомендованных фильмов для пользователя с id {}", userId);
+        Map<Long, Set<Long>> allUserLikes = likeDbStorage.getAllUserLikes(); // выгрузка таблицы likes в нужном формате
+
+        Set<Long> userLikes = allUserLikes.getOrDefault(userId, Collections.emptySet()); // проверка на наличие интересующего пользователя и его лайков
+        if (userLikes.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        Map<Long, Long> commonLikesCount = countCommonLikes(allUserLikes, userId, userLikes); // подсчёт общих лайков
+        Optional<Long> mostSimilarUser = findSimilarUser(commonLikesCount); // определение наиболее похожего пользователя
+        if (mostSimilarUser.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<Long> recommendedFilms = getRecommendedFilms(allUserLikes, userLikes, mostSimilarUser.get());
+        return recommendedFilms.stream().map(filmDbStorage::getById).toList();
+    }
+
+    private Map<Long, Long> countCommonLikes(Map<Long, Set<Long>> allUserLikes, long userId, Set<Long> userLikes) {
+        Map<Long, Long> counts = new HashMap<>(); // другой пользователь, общие лайки
+        allUserLikes.forEach((otherUserId, otherUserLikes) -> {
+            if (otherUserId != userId) {
+                long commonCount = otherUserLikes.stream()
+                        .filter(userLikes::contains)
+                        .count();
+                if (commonCount > 0) {
+                    counts.put(otherUserId, commonCount);
+                }
+            }
+        });
+        return counts;
+    }
+
+    private Optional<Long> findSimilarUser(Map<Long, Long> commonLikesCount) {
+        return commonLikesCount.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .filter(entry -> entry.getValue() > 0)
+                .map(Map.Entry::getKey);
+    }
+
+    private List<Long> getRecommendedFilms(Map<Long, Set<Long>> allUserLikes, Set<Long> userLikes, long similarUserId) {
+        Set<Long> similarUserLikes = new HashSet<>(allUserLikes.get(similarUserId));
+        similarUserLikes.removeAll(userLikes);
+        return new ArrayList<>(similarUserLikes);
+    }
+}
